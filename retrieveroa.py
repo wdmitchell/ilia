bca2a07349b4c34b3ab6f169598616d6124d5465
kimSooHyun950921 <soohyunkim950921@gmail.com>
2019-02-24 09:56:57 +0900

    add error handler

--- retrieveroa.py
+++ retrieveroa.py
@@ -7,7 +7,7 @@ import argparse
 import concurrent.futures
 import os
 
-poolCount = 10
+poolCount = 50
 
 
 def apnic():
@@ -24,70 +24,67 @@ def apnicvalidator(asn, prefix):
     r = requests.get("http://data1.labs.apnic.net:8080/api/v1/validity/{0}/{1}".format(asn,prefix))
     return r.json()
 
-def write_validity(route,validity):
-    try:
-        with open('validity_reason.csv','a') as f:
-            format = route["origin_asn"]+","+route["prefix"]+","+validity["reason"]
-            print(format)
-            f.write(format)
-    except FileNotFoundError:
-        with open('validity_reason.csv','w') as f:
-            format = route["origin_asn"]+","+route["prefix"]+","+validity["reason"]
-            print(format)
-            f.write(format)
+def write_validity(validity_list):
+    for validity in validity_list:
+        try:
+            print(validity_list)
+            with open('validity_reason1.csv','a') as f:
+                format ="{AS Number:"+ validity["route"]["origin_asn"]+", Prefix:"+validity["route"]["prefix"]+", Invalid Reason:"+validity["validity"]["reason"]+"}\r\n"
+                print(format)
+                f.write(format)
+        except FileNotFoundError:
+            with open('validity_reason1.csv','w') as f:
+                format ="{AS Number:"+ validity["route"]["origin_asn"]+", Prefix:"+validity["route"]["prefix"]+", Invalid Reason:"+validity["validity"]["reason"]+"}\r\n"
+                print(format)
+                f.write(format)
 
-
-'''def validating(data):
-    #print(data)
-    for roas in data['roas']:
-        print(roas['asn'], roas['prefix'], roas['maxLength'])
-        loadata =apnicvalidator(roas['asn'],roas['prefix'])
-        data = loadata["validated_route"]
-        route = data["route"]
-        validity= data["validity"]
-           # print("test:",validity)
-
-            #print("origin_asn : ", route["origin_asn"], "prefix:",route["prefix"])
-            #print("state:",validity["state"],"reason:",validity["description"])
-        validity_reason = list()
-        if validity["state"] == "Invalid":
-            print("reason",validity["reason"])
-            validity_reason.append(validity["reason"])
-
-    print("write_clear")
-
-'''
 
 def list_all_file():
     split_files = list()
-    for root,dirs,files in os.walk('data/'):
+    for root,dirs,files in os.walk('data1/'):
         split_files = files
     #print(split_files)
     return split_files
 
+def read_data(file):
+    ip_list = list()
+    print(file)
+    with open("data1/"+file,"r") as f:
+        while True:
+            line = f.readline()
+            if not line: break
+            line = line.replace('\n','')
+            split_data = line.split(' ')
+
+            ip_list.append({"ip":split_data[0],"as_num":split_data[1]})
+    return ip_list
 
 def is_validate(split_file):
-    print(split_file)
-    json_data = open("data/"+split_file,'r')
 
-    need_check_data = json.load(json_data)
-    validity_list = list()
-    for roas in need_check_data['roas']:
-        print(roas)
-        loadata =apnicvalidator(roas['asn'],roas['prefix'])
-        checked_data = loadata["validated_route"]
-        route = checked_data["route"]
-        validity = checked_data["validity"]
-        validity_list.append({"route":route,"validity":validity})
+    ip_list = read_data(split_file)
+    invalidity_list = list()
+    count = 0
+    load_data = ''
+    try:
+        for ip in ip_list:
 
-    for split_data in validity_list:
-        print("check:",split_data["route"])
-        if split_data["validity"]["state"] == "Invalid":
-                #if is not invalid
-            print("Reason:", validity["reason"])
-            return split_data, False
+            loadata =apnicvalidator("AS"+ip['as_num'],ip['ip'])
+            checked_data = loadata["validated_route"]
+            route = checked_data["route"]
+            validity = checked_data["validity"]
+            if validity["state"] == "Valid":
+                print("valid")
+            elif validity["state"] == "Invalid":
+                    #if is not invalid
+                invalidity_list.append(checked_data)
+                print("Reason:", validity["reason"])
+            else:
+                print("not Found")
+        print(count)
+    except:
+        print(load_data)
 
-        return split_data, True #if is VALID
+    return invalidity_list #if is VALID
 
 
 
@@ -96,14 +93,14 @@ def main():
 
     print("---")
     chunked_files = list_all_file()
-#    for split_file in files:
-
+    print(chunked_files)
+    count =0
     with concurrent.futures.ThreadPoolExecutor(max_workers=poolCount) as executor:
-        for data,is_valid in executor.map(is_validate,chunked_files):
+        for validity_list in executor.map(is_validate,chunked_files):
+            count+=50
+            print("count"+str(count))
         #    for data,is_valid in executor.map(is_validate,need_check_data['roas']):
-            if is_valid == False:
-                print("reason",data["validity"]["reason"], "valid", is_valid)
-                write_validity(data["route"],data["validity"])
+            write_validity(validity_list)
 
 
 
